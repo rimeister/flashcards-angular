019edbade1607b6d4726d9eff0e483e5c2fe4355
Riley Boyd <contact@rileyboyd.com>
2018-05-29 10:35:43 -0400

    Got deck component to iterate over each card in the JSON

--- src/app/add-cards.pipe.ts
+++ src/app/add-cards.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AddCardsPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+	transform(pipeData): any {
+    	return pipeData;
+	}
 
+	/*  transform(value: any, args?: any): any {
+	    return null;
+	  }
+	*/
 }
--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-deck',
   templateUrl: './deck.component.html'
 })
+
 export class DeckComponent implements OnInit {
 
-  constructor() { }
+	// This is how you update thing after the constructor; value is initialized in constructor, updated here.
+	cards: object; // Defining data types gives you better error control
 
-  ngOnInit() {
-  }
+	// Private variable of type HTTPClient
+	constructor( private http: HttpClient ) {
+		// "Components become more powerful when they allow you to work with data (e.g., data below)"
+		// Directives allow you to manipulate the data in order to display it within your template
+	}
+
+	ngOnInit(): void { // Void, so of no type request -- get, post, etc.
+		this.http.get<Object>('../assets/json/flashcard_QA.json').subscribe(
+			data => {
+				this.cards = data;
+		}) // Specify the type of 
+	}
 
 }
