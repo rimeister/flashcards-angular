bd434d47dec379fb6dc5298e17d4fd3bbbe6d12e
Riley Boyd <contact@rileyboyd.com>
2018-05-30 11:14:51 -0400

    Working on positioning cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  inputs: ['cardData']
+  inputs: ['cardData','index'] 
+  /* Because I used inputs here inside the 
+  @component, I don't have to use @input inside the class definition */
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+	cardData:object = this.cardData;	
+	index:number = this.index;	
 
-  ngOnInit() {
-  }
+
+	constructor() { }
+
+	ngOnInit() {
+		console.log(this.index);
+		this.updatePosition();
+	}
+
+	public updatePosition() {
+		return {
+			'left': '100px'
+		}
+	}
 
 }
--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -3,25 +3,36 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-deck',
-  templateUrl: './deck.component.html'
+  templateUrl: './deck.component.html',
+
 })
 
 export class DeckComponent implements OnInit {
 
 	// This is how you update thing after the constructor; value is initialized in constructor, updated here.
 	cards: object; // Defining data types gives you better error control
+	i: number;
 
 	// Private variable of type HTTPClient
 	constructor( private http: HttpClient ) {
 		// "Components become more powerful when they allow you to work with data (e.g., data below)"
 		// Directives allow you to manipulate the data in order to display it within your template
+		this.i = 0; // Create iterator var, to keep count of number of cards created in this deck.
 	}
 
 	ngOnInit(): void { // Void, so of no type request -- get, post, etc.
+
 		this.http.get<Object>('../assets/json/flashcard_QA.json').subscribe(
 			data => {
 				this.cards = data;
 		}) // Specify the type of 
+		
 	}
+
+/*
+	positionCards(): void {
+		// console.log(this.cards);
+	}
+*/
 
 }
