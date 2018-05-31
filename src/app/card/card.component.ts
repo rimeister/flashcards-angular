9fdf8554eee00cac908e5058c847cc2e71f4ab2f
Riley Boyd <contact@rileyboyd.com>
2018-05-31 10:30:04 -0400

    Card styles now position vertically using index value on margin-top

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  inputs: ['cardData','index','style.left.px'] 
+  inputs: ['cardData','index'] 
   /* Because I used inputs here inside the 
   @component, I don't have to use @input inside the class definition */
 })
@@ -11,12 +11,16 @@ export class CardComponent implements OnInit {
 
 	cardData:object = this.cardData;	
 	index:number = this.index;	
+	cardStyle:object;
 
 
-	constructor() { }
+	constructor() {
+		this.cardStyle = {'margin-top':'0px'}
+	}
 
 	ngOnInit() {
-
+		console.log(this.index+'px');
+		this.cardStyle['margin-top'] = this.index+'px';
 	}
 
 }
